54cf91e42bc49c3d826b7aee0ef988dfeee56e23
krewsona <ajkrewson@gmail.com>
2025-04-23 17:00:54 -0500

    updated practice - post solution

--- practice.js
+++ practice.js
@@ -25,6 +25,7 @@ class LibraryItem {
         }
         console.log(`\n${this.title} is ${status}.\n`)
      }
+     
 }
 
 //step 2: extend base class
@@ -35,15 +36,21 @@ class Book extends LibraryItem {
         this.genre = genre;
         this.isbn = isbn;
     }
+    getDetails() { //added this AFTER seeing soltution
+        return `${this.title}: written by ${this.author}. Genre: ${this.genre}. ISBN: ${this.isbn}.`
+     }
 }
 
-class DVD extends LibraryItem {
+class Dvd extends LibraryItem {
     constructor (title, isAvailable, genre, director, duration, upc) {
         super (title, isAvailable);
         this.genre = genre;
         this.director = director;
         this.duration = duration;
         this.upc = upc;
+    }
+    getDetails() { //added this after seeing soltution
+        return `${this.title}: Directed by: ${this.director}. Genre: ${this.genre}. Duration: ${this.duration}. UPC: ${this.upc}.`;
     }
 }
 
@@ -53,6 +60,9 @@ class Magazine extends LibraryItem {
         this.issueNummber = issueNumber;
         this.publisher = publisher;    
     }
+    getDetails() { //added this after seeing soltution
+        return `${this.title}: Published by: ${this.publisher}. Issue: ${this.issueNummber}.`;
+    }
 }
 
 //step 3: instantiate objects
@@ -60,31 +70,31 @@ let aCourtOfThornsAndRoses = new Book(`A Court of Thorns and Roses`, false, `Sar
 let atomicHabits = new Book(`Atomic Habits`, true, `James Clear`, `self help`, 9780735211292);
 let myBestFriendsExcorcism = new Book("My Best Friend`s Excorcism", true, `Gary Hendrix`, `horror; fiction`, 1594748624);
 
-let americanPsycho = new DVD(`American Psycho`, true, `horror`, `Mary Harron`, `190 minutes`, 48984269009);
-let twentyTwoJumpStreet = new DVD(`22 Jump Street`, false, `comedy`, `Phil Lord, Christopher Miller`, `112 minutes`, 43396439672);
-let tombstone = new DVD(`Tombstone`, true, `western`, `George Cosmatos`, `127 minutes`, 786936725247); 
+let americanPsycho = new Dvd(`American Psycho`, true, `horror`, `Mary Harron`, `190 minutes`, 48984269009);
+let twentyTwoJumpStreet = new Dvd(`22 Jump Street`, false, `comedy`, `Phil Lord, Christopher Miller`, `112 minutes`, 43396439672);
+let tombstone = new Dvd(`Tombstone`, true, `western`, `George Cosmatos`, `127 minutes`, 786936725247); 
 
 let sportsIllustrated = new Magazine(`Sports Illustrated`, true, `April 2025`, `Minute Media`);
 let theHockeyNews = new Magazine(`The Hockey News`, false, `April 2025`, `W.M. Kiplinger`);
 
 //step 4: test inheritance
 //managing availabilty 
-console.log(aCourtOfThornsAndRoses); //shows orignal listing
+// console.log(aCourtOfThornsAndRoses); //shows orignal listing
 aCourtOfThornsAndRoses.describe(); // displays that book is available
 aCourtOfThornsAndRoses.checkoutTitle(); //displays that book has now been checked out 
 
-console.log(aCourtOfThornsAndRoses); //shows updated listing, now availability is false
+// console.log(aCourtOfThornsAndRoses); //shows updated listing, now availability is false
 aCourtOfThornsAndRoses.describe(); //book is no longer available
 aCourtOfThornsAndRoses.returnTitle(); //displays book has been returned message
 
-console.log(aCourtOfThornsAndRoses); //listing reupdates and book is now available again
+// console.log(aCourtOfThornsAndRoses); //listing reupdates and book is now available again
 aCourtOfThornsAndRoses.describe(); // displays book is once again available 
 
 //accessing additional properties
-console.log(americanPsycho); //movies differ from books, removes author, genre, and isbn, adds genre, director, duration, and upc
-
-
-
+//I added this after seeing soltution
+console.log(americanPsycho.getDetails());
+console.log(aCourtOfThornsAndRoses.getDetails());
+console.log(sportsIllustrated.getDetails());
 
 
 
